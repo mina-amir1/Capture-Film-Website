f7a55274b9378babba66355b649c96d649bd6545
Mina Amir <mina.amir@scandiweb.com>
2022-02-06 17:47:06 +0200

    Styling the About section and adding the Global style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import AboutUs from './pages/AboutUs';
+import GlobalStyle from "./components/GlobalStyle";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,27 +1,54 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import styled from "styled-components";
 const AboutSection=()=>{
 
     return(
-        <div>
-            <div className="description">
+        <StyledAbout>
+            <StyledDescription>
                 <div className="title">
-                    <div className="hide">
+                    <StyledHide>
                         <h2>We work to make </h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2> your <span>dreams</span> come </h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide>
                         <h2>true.</h2>
-                    </div>
+                    </StyledHide>
                 </div>
                 <p>Contat us for any photography or videography ideas that you have.
                      We have professionals with amazing skills.  </p>
                 <button>Contact Us</button>
-            </div>
+            </StyledDescription>
+            <StyledImage>
             <img src={home1} alt="guy with camera" />
-        </div>
+            </StyledImage>
+        </StyledAbout>
     );
 }
+const StyledAbout = styled.div`
+min-height: 90vh;
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 5rem 10rem;
+color: white;
+`;
+const StyledDescription =styled.div`
+flex: 1;
+padding-right: 5rem;
+`;
+const StyledImage =styled.div`
+flex: 1;
+overflow: hidden;
+img{
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+}
+`;
+const StyledHide= styled.div`
+overflow: hidden;
+`;
 export default AboutSection;
